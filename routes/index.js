4407160f1b45291cca1c8014a2b0b2c50a59fa9b
sohail khan <sohailkhan4764@gmail.com>
2020-01-04 16:48:41 +0530

    setup static files

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 
-router.get('/',homeController.home)
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports = router;
